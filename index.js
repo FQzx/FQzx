63db91dd824a7dcb9aa9969dd30d1e5b7f6e4c4f
FQzx <faiz.wahyusetiawan@gmail.com>
2026-01-31 21:07:42 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//lenis 
+const lenis = new Lenis()
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+//Scroll To Fix
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
